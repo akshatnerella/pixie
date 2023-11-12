503aeeedb89d7dde87a74b09a4f5d025582a049e
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2023-11-12 17:09:34 -0600

    testservo

---
+++ code/testcode/servotest.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
+# Replace 17 with your chosen GPIO pin for the servo
+servo_pin = 17
+
+def center_servo(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+
+    pwm = GPIO.PWM(pin, 50)
+    pwm.start(0)
+
+    try:
+        duty_cycle = 7.5  # Adjust this value based on your servo's specifications
+        pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(1)
+
+    finally:
+        pwm.stop()
+        GPIO.cleanup()
+
+def sweep_servo(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+
+    pwm = GPIO.PWM(pin, 50)
+    pwm.start(0)
+
+    try:
+        for angle in range(0, 181, 10):  # Move in 10-degree steps
+            duty_cycle = angle / 18 + 2
+            pwm.ChangeDutyCycle(duty_cycle)
+            time.sleep(0.2)  # Adjust this value for the desired speed
+
+    finally:
+        pwm.stop()
+        GPIO.cleanup()
+
+# Test the centering function
+center_servo(servo_pin)
+
+# Test the sweeping function
+sweep_servo(servo_pin)
