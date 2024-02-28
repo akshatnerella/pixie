68165eaabf37840149bbe43fa37c664bf6b71beb
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-27 21:49:57 -0600

    updated testvideo.py

--- code/testcode/videotest.py
+++ code/testcode/videotest.py
@@ -1,2 +1,5 @@
 from subprocess import call
+import time
+call('cvlc testexpressions.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
+time.wait(10)
 call('cvlc testexpressions.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
