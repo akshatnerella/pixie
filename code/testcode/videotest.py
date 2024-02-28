5a7d24bc9f0ba3a33190e437742308a1c4bccecb
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-27 21:41:08 -0600

    lastest

--- code/testcode/videotest.py
+++ code/testcode/videotest.py
@@ -1,15 +1,2 @@
-import numpy as np
-import cv2
-
-cap = cv2.VideoCapture('testexpressions.mp4')
-
-while True:
-    ret, frame = cap.read()
-    cv2.namedWindow('output', cv2.WINDOW_NORMAL)  # Create a resizable window
-    cv2.setWindowProperty('output', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)  # Set the window to full screen
-    cv2.imshow('output', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+from subprocess import call
+call('cvlc testexpressions.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
