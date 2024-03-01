b198b55f8a02b0d8ddfe9418ea5843e1e1f560fa
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 23:08:14 -0600

    changes

---
+++ mian.py
@@ -0,0 +1,13 @@
+import subprocess
+import time
+import paths
+
+VLC_PARAMS = ' --fullscreen --repeat --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1'
+
+p = subprocess.Popen('cvlc' + paths.SAD + VLC_PARAMS, shell=True)
+time.sleep(2)
+p.terminate()
+time.sleep(0.01)
+x = subprocess.Popen('cvlc' + paths.ANGRY + VLC_PARAMS, shell=True)
+time.sleep(2)
+x.terminate()
