a2d99b1760b5a73e05a53ce4c1099a50e38afa34
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 23:42:47 -0600

    try

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import time
 import paths
 
 VLC_PARAMS_INIT = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --mouse-hide-timeout=1 --qt-fullscreen-screennumber=1'
-VLC_PARAMS_ADD = ' --playlist-enqueue'
+VLC_PARAMS_ADD = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --mouse-hide-timeout=1 --qt-fullscreen-screennumber=1 --playlist-enqueue'
 
-p = subprocess.Popen('cvlc' + paths.NEUTRAL + VLC_PARAMS_INIT, shell=True)
-time.sleep(3)
+p = subprocess.Popen('cvlc' + VLC_PARAMS_ADD + paths.LOVE, shell=True)
+#time.sleep(1)
 #p.terminate()
 #time.sleep(0.001)
 x = subprocess.Popen('cvlc' + VLC_PARAMS_ADD + paths.ANGRY, shell=True)
