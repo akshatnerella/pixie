b8b120c1ed1d5b49c725423dbbc8d5d5698d10f6
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 22:00:56 -0600

    new method

--- videotest.py
+++ videotest.py
@@ -1,9 +1,13 @@
-import subprocess
 import time
-p = subprocess.Popen('vlc -L -f assets/expressions/sad.mp4', shell=True)
-time.sleep(2)
-p.terminate()
-time.sleep(0.01)
-x = subprocess.Popen('cvlc assets/expressions/love.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
-time.sleep(2)
-x.terminate()
+import vlc
+
+instance = vlc.Instance("--verbose=0","--no-xlib", "--no-video-title-show", "--no-osd", "--no-snapshot-preview", "--vout mmal_vout")
+media_list = instance.media_list_new(['/assets/expressions/angry.mp4', 'assets/expressions/excited.mp4'])
+list_player = instance.media_list_player_new()
+list_player.set_media_list(media_list)
+
+list_player.set_playback_mode(vlc.PlaybackMode.loop)
+list_player.play()
+
+while True:
+    time.sleep(300)
