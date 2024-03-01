c4bd52df4d6b448f02144471fd4608bcdcc549fd
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 23:29:18 -0600

    mouse

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import subprocess
 import time
 import paths
 
-VLC_PARAMS = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1'
+VLC_PARAMS = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --mouse-hide-timeout=1 --qt-fullscreen-screennumber=1'
 
 p = subprocess.Popen('cvlc' + paths.SAD + VLC_PARAMS, shell=True)
 time.sleep(5)
-p.terminate()
+#p.terminate()
 time.sleep(0.001)
 x = subprocess.Popen('cvlc' + paths.ANGRY + VLC_PARAMS, shell=True)
 time.sleep(5)
+p.terminate()
 x.terminate()
