7bcbd1a9e0bb770824e2d9c0abba81a7a580209d
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 23:45:29 -0600

    more wiat

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ VLC_PARAMS_INIT = ' --fullscreen --no-video-title-show --no-audio --no-embedded-
 VLC_PARAMS_ADD = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --mouse-hide-timeout=1 --qt-fullscreen-screennumber=1 --playlist-enqueue'
 
 p = subprocess.Popen('cvlc' + VLC_PARAMS_ADD + paths.LOVE, shell=True)
-time.sleep(1)
+time.sleep(10)
 #p.terminate()
 #time.sleep(0.001)
 x = subprocess.Popen('cvlc' + VLC_PARAMS_ADD + paths.ANGRY, shell=True)
