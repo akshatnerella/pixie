4d0cc7d205b56266d5aa67f6b0c34cd17831c78c
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 22:55:48 -0600

    params edit

--- videotest.py
+++ videotest.py
@@ -1,9 +1,12 @@
 import subprocess
 import time
-p = subprocess.Popen('cvlc assets/expressions/sad.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1 --vout mmal_vout', shell=True)
+
+params = '--fullscreen --repeat --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1 --vout mmal_vout'
+
+p = subprocess.Popen('cvlc assets/expressions/sad.mp4' + params, shell=True)
 time.sleep(2)
 p.terminate()
 time.sleep(0.01)
-x = subprocess.Popen('cvlc assets/expressions/love.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1 --vout mmal_vout', shell=True)
+x = subprocess.Popen('cvlc assets/expressions/love.mp4' + params, shell=True)
 time.sleep(2)
 x.terminate()
