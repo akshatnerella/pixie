7a4b4a2d60485cad2f52c882f158b92c7b37141f
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 21:47:00 -0600

    test

--- videotest.py
+++ videotest.py
@@ -3,6 +3,7 @@ import time
 p = subprocess.Popen('cvlc assets/expressions/sad.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
 time.sleep(2)
 p.terminate()
+time.sleep(0.01)
 x = subprocess.Popen('cvlc assets/expressions/love.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
 time.sleep(2)
 x.terminate()
