5d4a3d132439fba5a23d0734e880d785cea98352
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 23:23:50 -0600

    no repeat

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 import paths
 
-VLC_PARAMS = ' --fullscreen --repeat --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1'
+VLC_PARAMS = ' --fullscreen --no-video-title-show --no-audio --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1'
 
 p = subprocess.Popen('cvlc' + paths.SAD + VLC_PARAMS, shell=True)
 time.sleep(2)
