0c8f1509b1a80f3c325daa72021f13c3006c0eae
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 21:24:47 -0600

    added expressions

--- code/testcode/videotest.py
+++ code/testcode/videotest.py
@@ -1,5 +1,5 @@
 from subprocess import call
 import time
 call('cvlc testexpressions.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
-time.wait(10)
+time.sleep(1000)
 call('cvlc testexpressions.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
