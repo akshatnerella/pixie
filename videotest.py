33bd48a423ee8f3f1c94946c0edd8833bd91c84c
Akshat Nerella <91713635+akshatnerella@users.noreply.github.com>
2024-02-29 21:49:34 -0600

    vlc instead of cvlc

--- videotest.py
+++ videotest.py
@@ -1,6 +1,6 @@
 import subprocess
 import time
-p = subprocess.Popen('cvlc assets/expressions/sad.mp4 --fullscreen --no-video-title-show --no-embedded-video --no-qt-fs-controller --qt-fullscreen-screennumber=1', shell=True)
+p = subprocess.Popen('vlc -L -f assets/expressions/sad.mp4', shell=True)
 time.sleep(2)
 p.terminate()
 time.sleep(0.01)
